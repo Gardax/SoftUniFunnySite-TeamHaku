22bcd11430f5cd1d594ea1904b63fb19bcb53161
Georgi <gardax95@gmail.com>
2014-06-13 03:32:36 +0300

    Added index page and effects when open the site

---
+++ scripts/mainScript.js
@@ -0,0 +1,19 @@
+
+$(document).ready(attachEvents);
+
+function attachEvents() {
+    openTheSite();
+}
+
+function openTheSite() {
+    $("#btnOpenSite").on("click", function () {
+        $("#btnOpenSiteContainer").css("display", "none");
+        var topElementHeight = $("#topImageContainer").height();
+        var bottomElementHeight = $("#bottomImageContainer").height();
+        $("#topImageContainer").animate({ top: -topElementHeight * 0.4, opacity: "show" }, 1500);
+        $("#bottomImageContainer").animate({ top: bottomElementHeight* 0.45, opacity: "show" }, 1500, function () {
+            
+        });
+        
+    });
+}
